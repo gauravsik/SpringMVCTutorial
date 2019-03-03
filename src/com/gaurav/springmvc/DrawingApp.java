9a50b4e7c5276b9e3cd944a74c64273c1e602f93
Gaurav <kgaurav9singh@gmail.com>
2019-03-03 12:17:39 +0530

    Bean Lifecycle Callbacks.

--- src/com/gaurav/springmvc/DrawingApp.java
+++ src/com/gaurav/springmvc/DrawingApp.java
@@ -1,6 +1,6 @@
 package com.gaurav.springmvc;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
@@ -9,9 +9,10 @@ public class DrawingApp {
 		//Triangle triangle = new Triangle();
 		
 		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		//Triangle triangle = (Triangle) factory.getBean("triangle");
-		Triangle triangle = (Triangle) context.getBean("triangle1");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();  
 	}
 	
--- src/com/gaurav/springmvc/Triangle.java
+++ src/com/gaurav/springmvc/Triangle.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class Triangle {
+public class Triangle implements InitializingBean, DisposableBean {
 
 	private List<Point> points;
 
@@ -25,5 +27,23 @@ public class Triangle {
 			System.out.print("Triangle co-ordinates ( " + point.getX() + ", " + point.getY() + ")\n");
 
 		}
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Disposable Beans destroy method");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Initializing Beans init method");
+	}
+	
+	public void myInit() {
+		System.out.println("My init method runs");
+	}
+	
+	public void myCleanUp() {
+		System.out.println("My cleanUp method runs");
 	}
 }
