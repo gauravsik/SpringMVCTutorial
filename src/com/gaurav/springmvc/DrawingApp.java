730ef3ad648081aa5530451391de204340a4ebf0
Gaurav <kgaurav9singh@gmail.com>
2019-03-03 14:19:24 +0530

    RequiredBeanPostProcessor implementation

--- src/com/gaurav/springmvc/DrawingApp.java
+++ src/com/gaurav/springmvc/DrawingApp.java
@@ -7,22 +7,9 @@ public class DrawingApp {
 
 	public static void main(String args[]) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		
-		System.out.println("*****Coding to Implementation*****");
-		
-		Triangle triangle = (Triangle) context.getBean("triangle");
-		triangle.draw();
-		Circle circle = (Circle) context.getBean("circle");
-		circle.draw();
-		
-		System.out.println("*****Coding to Interfaces*****");
-		
-		Shape shape1 = (Shape) context.getBean("triangle");
-		shape1.draw();
-		
-		Shape shape2 = (Shape) context.getBean("circle");
-		shape2.draw();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");		
+		Shape shape = (Shape) context.getBean("circle");
+		shape.draw();
 	}
 
 }
