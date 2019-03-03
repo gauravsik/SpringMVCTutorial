901443a72ad472817a88534a45ab8d84b80a8626
Gaurav <kgaurav9singh@gmail.com>
2019-03-03 11:23:31 +0530

    Bean Inheritance: Through individual properties

--- src/com/gaurav/springmvc/Triangle.java
+++ src/com/gaurav/springmvc/Triangle.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class Triangle implements ApplicationContextAware, BeanNameAware{
+public class Triangle {
 	
 	private Point pointA;
 	private Point pointB;
@@ -36,18 +36,6 @@ public class Triangle implements ApplicationContextAware, BeanNameAware{
 		System.out.println("Point B = (" + getPointB().getX() + "," + getPointB().getY() + ")");
 		System.out.println("Point C = (" + getPointC().getX() + "," + getPointC().getY() + ")");
 	}
-	
-	@Override
-	public void setApplicationContext(ApplicationContext context) throws BeansException {
-		this.context = context;
 		
-	}
-	@Override
-	public void setBeanName(String beanName) {
-		System.out.println("Bean name is " + beanName);
-		
-	}
-	
-	
 	
 }
